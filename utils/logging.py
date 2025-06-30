eeb7eb38625e15b6c52a5de5b4e07ca63b847641
demoner21 <a.tranhago@gmail.com>
2025-06-30 16:20:05 -0300

    feat(logger): add logger to handle request and application

---
+++ utils/logging.py
@@ -0,0 +1,29 @@
+import structlog
+import logging
+
+def setup_logging():
+    """
+    Configura o logging para o projeto.
+    """
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    
+    structlog.configure(
+        processors=[
+            structlog.stdlib.filter_by_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.JSONRenderer(),
+            structlog.contextvars.merge_contextvars,
+        ],
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+    
+    return logging.getLogger(__name__)
+
+logger = setup_logging()
