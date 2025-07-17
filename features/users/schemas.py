395f902ac9f1c56d83b29032887d0411103e8161
demoner21 <a.tranhago@gmail.com>
2025-07-16 21:57:34 -0300

    feat(schemas): Defines schemas for updating user data

--- features/users/schemas.py
+++ features/users/schemas.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from typing import Optional
 
 
 class UserCreate(BaseModel):
@@ -6,3 +7,26 @@ class UserCreate(BaseModel):
     email: str
     senha: str
     confirmar_senha: str
+
+class PasswordUpdate(BaseModel):
+    """Schema para a requisição de atualização de senha."""
+    senha_atual: str
+    nova_senha: str
+
+class UserUpdate(BaseModel):
+    """Schema para a requisição de atualização de dados do usuário."""
+    nome: Optional[str] = None
+    email: Optional[EmailStr] = None
+
+    class Config:
+        from_attributes = True
+
+class UserResponse(BaseModel):
+    """Schema para a resposta de dados do usuário (sem a senha)."""
+    id: int
+    nome: str
+    email: EmailStr
+    role: str
+
+    class Config:
+        from_attributes = True
